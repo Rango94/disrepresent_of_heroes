b9fabf607dfe0cd99e451cb547241a4f636ea71a
wangnanzhi <wangnanzhi@zbj.com>
2018-03-06 14:50:41 +0800

    another paper of mine,so sad.

--- src/test/java/buildmap.java
+++ src/test/java/buildmap.java
@@ -9,18 +9,29 @@ public class buildmap {
     List<String> neg=new ArrayList<String>();
     HashMap<String,Integer> cor_pos=new HashMap<String, Integer>();
     HashMap<String,Integer> cor_neg=new HashMap<String, Integer>();
+    HashMap<String,Double> dou_pos=new HashMap<String, Double>();
+    HashMap<String,Double> dou_neg=new HashMap<String, Double>();
+    int size;
+
 
     public buildmap(double[] line,int size){
+        this.size=size;
+
         double pos_int=0;
-        HashMap<String,Double> tmp_pos=new HashMap<String, Double>();
         for(int i=0;i<line.length;i++){
-            if(line[i]>0){
-                tmp_pos.put(Integer.toString(i+1),line[i]);
-                line[i]+=pos_int;
+            if(i==23){
+                continue;
             }
+            if(line[i]>0){
+                dou_pos.put(Integer.toString(i+1),line[i]);
+                pos_int+=line[i];
+            }
+            ;
         }
-        for(String e:tmp_pos.keySet()){
-            cor_pos.put(e,(int)(size*(tmp_pos.get(e)/pos_int)));
+        for(String e:dou_pos.keySet()){
+//            System.out.println(e+"\t"+Double.toString(dou_pos.get(e)));
+
+            cor_pos.put(e,(int)(size*(dou_pos.get(e)/pos_int)));
         }
         for(String e:cor_pos.keySet()){
             for(int i=0;i<cor_pos.get(e);i++){
@@ -29,15 +40,17 @@ public class buildmap {
         }
 
         double neg_int=0;
-        HashMap<String,Double> tmp_neg=new HashMap<String, Double>();
         for(int i=0;i<line.length;i++){
+            if(i==23){
+                continue;
+            }
             if(line[i]<=0){
-                tmp_neg.put(Integer.toString(i+1),line[i]);
-                line[i]+=neg_int;
+                dou_neg.put(Integer.toString(i+1),line[i]);
+                neg_int+=line[i];
             }
         }
-        for(String e:tmp_neg.keySet()){
-            cor_neg.put(e,(int)(size*(tmp_neg.get(e)/neg_int)));
+        for(String e:dou_neg.keySet()){
+            cor_neg.put(e,(int)(size*(dou_neg.get(e)/neg_int)));
         }
         for(String e:cor_neg.keySet()){
             for(int i=0;i<cor_neg.get(e);i++){
@@ -46,7 +59,51 @@ public class buildmap {
         }
     }
 
+    public List<String> pickpos(String hero,int num){
+        List<String> out=new ArrayList<String>();
+        for(int i=0;i<num;i++){
+            int tmppick=(int)(Math.random()*pos.size());
+            String id=pos.get(tmppick);
+            while(id.equals(hero)||out.contains(id)){
+                tmppick=(int)(Math.random()*pos.size());
+                id=pos.get(tmppick);
+            }
+            boolean flag=true;
+            for(int j=0;j<out.size();j++){
+                if(dou_pos.get(id)<dou_pos.get(out.get(j))){
+                    out.add(j,id);
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag){
+                out.add(id);
+            }
+        }
+        return out;
+    }
 
-
-
+    public List<String> pickneg(String hero,int num){
+        List<String> out=new ArrayList<String>();
+        for(int i=0;i<num;i++){
+            int tmppick=(int)(Math.random()*neg.size());
+            String id=neg.get(tmppick);
+            while(id.equals(hero)||out.contains(id)){
+                tmppick=(int)(Math.random()*neg.size());
+                id=neg.get(tmppick);
+            }
+            boolean flag=true;
+            for(int j=0;j<out.size();j++){
+                if(dou_neg.get(id)<dou_neg.get(out.get(j))){
+                    out.add(j,id);
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag){
+                out.add(id);
+            }
+        }
+        return out;
+    }
 }
--- src/test/java/genareteCorpus.java
+++ src/test/java/genareteCorpus.java
@@ -4,13 +4,15 @@ import java.util.HashMap;
 import java.util.List;
 
 public class genareteCorpus {
+
     public static void main(String[] args){
-        genareteCorpus();
+        genareteCorpus("E:/disrepresent_of_heroes/anti.txt","E:/disrepresent_of_heroes/anticorpus.txt",3,10000,false);
+        genareteCorpus("E:/disrepresent_of_heroes/comb.txt","E:/disrepresent_of_heroes/combcorpus.txt",3,10000,false);
     }
-    public void genareteCorpus(String path,int window,int length,boolean chaos){
-        Double[][] cor=new Double[114][114];
+    public static void genareteCorpus(String inpath,String outpath,int window,int length,boolean chaos){
+        double[][] cor=new double[114][114];
         try {
-            FileReader corpus=new FileReader(path);
+            FileReader corpus=new FileReader(inpath);
             BufferedReader R=new BufferedReader(corpus);
             for(int i=0;i<114;i++){
                 int count=0;
@@ -19,32 +21,89 @@ public class genareteCorpus {
                     count++;
                 }
             }
+            R.close();
         }catch (IOException e){
             e.printStackTrace();
         }
         List<List<String[]>> output=new ArrayList<List<String[]>>();
         for(int i=0;i<114;i++){
-            String[] tmpline=new String[window*2+1];
+            if(i==23){
+                continue;
+            }
+            System.out.println(i+1);
+            List<String[]> suboutput=new ArrayList<String[]>();
+            buildmap bm=new buildmap(cor[i],10000);
             for(int j=0;j<length;j++){
+                String[] tmpline=new String[window*2+1];
+                List<String> bm_pos=bm.pickpos(Integer.toString(i+1),3);
+                List<String> bm_neg=bm.pickneg(Integer.toString(i+1),3);
                 tmpline[window]=Integer.toString(i+1);
+                for(int k=0;k<tmpline.length;k++){
+                    if(k<window){
+                        tmpline[k]=bm_pos.get(k);
+                    }
+                    if(k>window){
+                        tmpline[k]=bm_neg.get(k-window-1);
+                    }
+                }
+
+//                for(String e:tmpline){
+//                    System.out.print(e+'\t');
+//                }
+//                System.out.print("\n");
+//                for(int p=0;p<tmpline.length;p++){
+//                    if(p<window) {
+//                        System.out.print(bm.dou_pos.get(tmpline[p])+"\t");
+//                    }
+//                    if(p>window){
+//                        System.out.print(bm.dou_neg.get(tmpline[p])+"\t");
+//                    }
+//                    if(p==window){
+//                        System.out.print("0"+"\t");
+//                    }
+//                }
+//                System.out.print("\n");
 
 
+                suboutput.add(tmpline);
+            }
+            output.add(suboutput);
+
+        }
+        if(chaos){
+            try {
+                FileWriter F = new FileWriter(outpath);
+                BufferedWriter W = new BufferedWriter(F);
+                for(int i=0;i<114*length;i++){
+                    List<String[]> heroline=output.get((int)(Math.random()*output.size()));
+                    for(String e:heroline.get((int)(heroline.size()*Math.random()))){
+                        W.write(e+'\t');
+                    }
+                    W.write("\n");
+                }
+                W.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }else{
+            try {
+                FileWriter F = new FileWriter(outpath);
+                BufferedWriter W = new BufferedWriter(F);
+                for (List<String[]> eachhero : output) {
+                    for (String[] line : eachhero) {
+                        for (String e : line) {
+                            W.write(e+"\t");
+                        }
+                        W.write("\n");
+                    }
+                }
+                W.close();
+            }catch (IOException e){
+                e.printStackTrace();
             }
         }
 
 
+
     }
-
-    String[] positive(Double[] line,int window){
-        HashMap<String,Double> pos=new HashMap<String, Double>();
-        for(int i=0;i<line.length;i++){
-            if(line[i]>0){
-                pos.put(Integer.toString(i+1),line[i]);
-            }
-        }
-    }
-
-
-
-
 }
